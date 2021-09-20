65df3f83e2bf6654e68db941f75299e32c98ec1f
satur123 <45348888+satur123@users.noreply.github.com>
2021-09-20 10:58:24 +0530

    Add files via upload

---
+++ display-engine.php
@@ -0,0 +1,50 @@
+<?php
+function display_query($Query, $Result_Type) {
+	$con=mysqli_connect("localhost","root","jed833","movies");
+	
+	//display the query
+	echo "<div class='panel panel-default'><div class='panel-body'>" . $Query . "</div></div>";
+
+	
+	//query the database
+	$result = mysqli_query($con, $Query);
+
+	echo "<table class='table'>";
+	
+	switch(table_type($Result_Type)) {
+		case "Person":
+			echo "<tr><th>First Name</th> <th>Last Name</th> <th>Birth Date</th> <th>Death Date</th></tr>";
+			while($row = mysqli_fetch_array($result)) {
+				display_row(array("first_name", "last_name", "birth_date", "death_date"), $row);
+			}
+			break;
+
+		case "Movie":
+			echo "<tr><th>Title</th> <th>Release Date</th> <th>Rating</th> <th>Length</th> <th>Tagline</th> <th>Summary</th> <th>Budget</th></tr>";
+			while($row = mysqli_fetch_array($result)) {
+				display_row(array("title", "release_date", "rating", "length", "tagline", "summary", "budget"), $row);
+			}
+                        break;
+                case "Critiques":
+                        echo "<tr><th>Written By</th> <th></th> <th>For Movie</th> <th>Stars Given</th> <th>Critique</th></tr>";
+                        while( $row = mysqli_fetch_array( $result ) ) {
+                                display_row( array( "first_name", "last_name", "title", "stars", "content" ), $row );
+                        }
+                        break;
+
+		default:
+			break;
+	}
+	echo "</table>";
+
+	mysqli_close($con);
+}
+
+function display_row($fields, $row) {
+	echo "<tr>";
+	foreach ($fields as $field) {
+		echo "<td>" . $row[$field] . "</td>";
+  	}
+  	echo "</tr>";
+}
+?>
